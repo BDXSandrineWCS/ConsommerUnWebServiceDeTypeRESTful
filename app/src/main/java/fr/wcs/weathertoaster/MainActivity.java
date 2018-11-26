3560fc46582644ee56bc13ec1aa66273e892ab79
sandrine <poupardsandrine@gmail.com>
2018-11-26 14:44:50 +0100

    Exécuter une requête vers une API step finished

--- app/src/main/java/fr/wcs/weathertoaster/MainActivity.java
+++ app/src/main/java/fr/wcs/weathertoaster/MainActivity.java
@@ -2,12 +2,55 @@ package fr.wcs.weathertoaster;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.lang.reflect.Method;
+import java.util.concurrent.Callable;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String API_KEY = "910932b0abccc3a557c66b2addd5e44f";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getObjectByJsonByVolley(getString(R.string.api_url)+API_KEY);
+    }
+
+    public void getObjectByJsonByVolley(String url) {
+
+        // Crée une file d'attente pour les requêtes vers l'API at url
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        // Création de la requête vers l'API, ajout des écouteurs pour les réponses et erreurs possibles
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        //TODO
+                    }
+                },
+                new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Afficher l'erreur
+                        Log.d("VOLLEY_ERROR", "onErrorResponse: " + error.getMessage());
+                    }
+                }
+        );
+        // On ajoute la requête à la file d'attente
+        requestQueue.add(jsonObjectRequest);
     }
 }
